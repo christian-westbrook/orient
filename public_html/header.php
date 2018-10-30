26fd52b0f3dcda074bc7c91a116b59e0f16fbb7f
Christian Westbrook <christianwestbrook@live.com>
2018-10-30 17:01:56 -0500

    Updated header.php and header.css
    Changed the h1 to a paragraph and combined some style settings

--- public_html/header.php
+++ public_html/header.php
@@ -4,7 +4,7 @@
 </head>
 
 <div id="header">
-	<a href="index.php"><h1>ORIENT</h1></a>
+	<a href="index.php"><p>ORIENT</p></a>
 	<a href="profile.php"><p>Profile</p></a>
 	<a href="settings.php"><p>Settings</p></a>
 	<a href="authenticate.php"><p>Log In</p></a>
