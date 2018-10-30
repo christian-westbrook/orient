456edc856a087cceb82299def9dc956348390f6c
Nathan Brown <44622445+thebrownnathan@users.noreply.github.com>
2018-10-30 17:25:27 -0500

    Updated index.php
    Made head changes

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'header.php';
+?>
+
 <head>
-<title>Page Title</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-body {
-    font-family: Arial, Helvetica, sans-serif;
+
+.newdiv{
+	align: center;
+}
+.img-circle{
+    border: solid 1px black;
+    position: fixed;
+    left: 50%;
+    top: 70%;
+    background-color: white;
+    z-index: 100;
+
+    height: 400px;
+    margin-top: -200px;
+
+    width: 600px;
+    margin-left: -300px;
 }
 </style>
+
 </head>
 <body>
 
-<h1>My Website</h1>
-<p>A website created by me.</p>
+<div class="container">
+  <div class="jumbotron">
+    <h1>Welcome to ORIENT</h1>
+    <p>Resize this responsive page to see the effect!</p> 
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+<div class ="newdiv">
+	<div class="myimage">
+	  <img src="research.jpg" class="img-circle" alt="Logo">
+</div>
 
 </body>
 </html>
