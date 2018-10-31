1f33d99ecaec487ba7c1b28bcc980b5e3ca84ade
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 15:54:00 -0500

    Updated index.css
    Adjusted index styles

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,8 @@ $css = array(
 include 'header.php';
 ?>
 
-<div id="background">
+<div id="container">
+	<img src="img/uafs-tower.jpg" />
 	<h1>ORIENT</h1>
 	<h3>The future of university research collaboration.</h3>
 </div>
