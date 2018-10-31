cfaebfb326f4e3635b6ba97fd61af8eca8c1d185
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 18:27:21 -0500

    Updated authenticate

--- public_html/authenticate.php
+++ public_html/authenticate.php
@@ -8,15 +8,17 @@ include 'header.php';
 
 ?>
 
-<div id=container>
+<div id="container">
 
-<form>
-	Username</br>
-	<input type="text" name="username"></br>
-	Password</br>
-	<input type="password" name="password"></br>
-</form>
-
+	<div id="log-in">
+		<p>Log In</p>
+		<form>
+			Username</br>
+			<input type="text" name="username"></br>
+			Password</br>
+			<input type="password" name="password"></br>
+		</form>
+	</div>
 </div>
 
 <?php include 'footer.php'; ?>
