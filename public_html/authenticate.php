1bdd5faccf2ec48161108dddc634306ed339c816
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 18:43:57 -0500

    Updated auth

--- public_html/authenticate.php
+++ public_html/authenticate.php
@@ -12,10 +12,6 @@ include 'header.php';
 	<div id="auth">
 		<h1>ORIENT</h1>
 		<p>The future of university research collaboration</p>
-		
-		<div id="get-started">
-		<a href="signup.php"><p>Get Started</p></a>
-		</div>
 	</div>
 </div>
 
