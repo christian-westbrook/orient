bb8508a008f1849bdf225b27df6ba6edb23e04e6
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 18:49:08 -0500

    Updated auth

--- public_html/authenticate.php
+++ public_html/authenticate.php
@@ -12,7 +12,12 @@ include 'header.php';
 	<div id="auth">
 		<p>Log In</p>
 		
-		
+		<form>
+			Username</br>
+			<input type="text" name="username" /></br>
+			Password
+			<input type="password" name="password" /><br>
+		</form>
 	</div>
 </div>
 
