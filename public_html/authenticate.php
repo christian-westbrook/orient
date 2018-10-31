0175b8337bb8688e1061de44a5bc6828bb711fba
Christian Westbrook <christianwestbrook@live.com>
2018-10-31 18:25:49 -0500

    Updated authenticate

--- public_html/authenticate.php
+++ public_html/authenticate.php
@@ -9,6 +9,14 @@ include 'header.php';
 ?>
 
 <div id=container>
+
+<form>
+	Username</br>
+	<input type="text" name="username"></br>
+	Password</br>
+	<input type="password" name="password"></br>
+</form>
+
 </div>
 
-<?php include 'footer'; ?>
+<?php include 'footer.php'; ?>
