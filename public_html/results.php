43efacb8a10a08cabf58f2037c561d9ffa63ba72
Christian Westbrook <christianwestbrook@live.com>
2018-11-01 10:29:22 -0500

    Updated results.php

---
+++ public_html/results.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************************************
+ * System	: Open-source Research Interest Network
+ * Version	: Prototype System II
+ * File		: results.php
+ * Developers 	: Christian Westbrook
+ *
+ * Abstract 	: 
+ **************************************************************************/
+
+$css = array(
+		0 => 'results'
+	    ); 
+
+include('header.php');
+
+ ?>
+
+<div id="container">
+	<div id="results">
+		
+	</div>
+</div>
+
+</body>
+
+<!-- Defines the footer of each page -->
+<?php include('footer.php'); ?>
