5c3770fcdf1f77ad9334bb1a786f8bcaafec1b86
Christian Westbrook <christianwestbrook@live.com>
2018-11-01 15:28:34 -0500

    Updated connection details

--- public_html/php/database.php
+++ public_html/php/database.php
@@ -5,7 +5,7 @@
 		$username = 'cwestbrook';
 		$password = 'orientdb';
 
-		$conn = new PDO("mysql:host=$host;dbname=orientdb", $username, $password);
+		$conn = new PDO("mysql:host=$host;dbname=ORIENT", $username, $password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e)
