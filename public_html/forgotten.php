f35667ea60a4e568278284c3cce093514bb6382a
Nathan Brown <44622445+thebrownnathan@users.noreply.github.com>
2018-11-01 16:05:38 -0500

    forgotten.php
    PHP page in order to get forgotten password

---
+++ public_html/forgotten.php
@@ -0,0 +1,25 @@
+<?php
+
+$css = array(
+		0 => 'auth'
+	    );
+			
+include 'header.php';
+
+?>
+
+<div id="container">
+	<div id="auth">
+		<p id="label">ORIENT</p>
+		
+		<form action="php/login-script.php" method="post">
+			<input type="text" name="email" placeholder="Email" class="field" /></br>
+			<input type="password" name="password" placeholder="Password" class="field" /><br>
+			<input type="submit" value="Log In" id="sub-button">
+		</form>
+		
+		<a href="auth.php"><p class="sub">Want to sign in?</p></a>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
