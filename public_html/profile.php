00514d0c1081effb0f4469451791594c04b70c01
Christian Westbrook <christianwestbrook@live.com>
2018-11-01 10:26:39 -0500

    Updated profile.php

---
+++ public_html/profile.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************************************
+ * System	: Open-source Research Interest Network
+ * Version	: Prototype System II
+ * File		: profile.php
+ * Developers 	: Christian Westbrook
+ *
+ * Abstract 	: 
+ **************************************************************************/
+
+$css = array(
+		0 => 'profile'
+	    ); 
+
+include('header.php');
+
+ ?>
+
+<div id="container">
+	<div id="profile">
+		
+	</div>
+</div>
+
+</body>
+
+<!-- Defines the footer of each page -->
+<?php include('footer.php'); ?>
