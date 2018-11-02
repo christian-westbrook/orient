232d164a82b84f9edfcb73762c79c616d0d99956
Christian Westbrook <christianwestbrook@live.com>
2018-11-01 22:26:35 -0500

    Added error message to login-script.php

--- public_html/php/login-script.php
+++ public_html/php/login-script.php
@@ -1,18 +1,27 @@
 <?php
+
+	// Get the user information provided through POST
 	$email      = $_POST['email'];
 	$plaintext  = $_POST['password'];
 
+	// Encrypt the provided plaintext password
 	$ciphertext = password_hash($plaintext, PASSWORD_DEFAULT);
-	
+
+
 	include 'database.php';
 	$sql  = 'SELECT USER_ID FROM USERS WHERE EMAIL=":EMAIL" AND PASSWORD=":PASSWORD"';
 
 	$stmt = $conn->prepare($sql);
 	$stmt->bindParam(':EMAIL', $email, PDO::PARAM_STR);
 	$stmt->bindParam(':PASSWORD', $plaintext, PDO::PARAM_STR);
-	$stmt->execute();
 
-	$results = $fetchAll();
-	
-	print_r($results);
+	if($stmt->execute())
+	{
+			$results = $fetchAll();
+			print_r($results);
+	}
+	else
+	{
+		echo "Statement failed to execute.";
+	}
 ?>
