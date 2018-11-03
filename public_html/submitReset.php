b0f498641c900fe1166d27d5801ca756d3439d5a
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-03 12:19:42 -0500

    Update submitReset.php

--- public_html/submitReset.php
+++ public_html/submitReset.php
@@ -1,14 +1,12 @@
 <?php
-	//if(!isset($_POST['submit']))
-	//{
-	//	echo "error; you need to submit the form!";
-	//}
-	echo "we are before";
-	//$email = $_POST['email'];
-	//$link = "espn.com";
-	//$subject = "Password reset request";
-	//$body = "Here is the link to reset your password.\n\n";
-	//mail($email,$subject,$body);
-	//header('Location: auth.php');
-	echo "we are after";
+	if(!isset($_POST['submit']))
+	{
+		echo "error! you need to submit the form!";
+	}
+	$email = $_POST['email'];
+	$link = "espn.com";
+	$subject = "Password reset request";
+	$body = "Here is the link to reset your password.\n\n";
+	mail($email,$subject,$body);
+	header('Location: auth.php');
 ?>
