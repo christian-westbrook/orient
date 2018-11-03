0066aa9d5f9e68b8cdc86fdac05adab4741f5bb9
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-03 11:44:43 -0500

    Update signup-script.php

--- public_html/php/signup-script.php
+++ public_html/php/signup-script.php
@@ -5,7 +5,7 @@ if($_POST['password']==$_POST['confirm']){
   //are we sending email to admins to confirm before doing sql insert
   //in either case, there has to somehow be a dept_id (this could be used as a means to confirm or not)
 	
-  $email = &_POST['email'];
+  $email = $_POST['email'];
   $password = $_POST['password'];
   
   echo "Email: ".$email."<br>Password: ".$password;
