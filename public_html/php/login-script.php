1232aee7866e1b97da705b51b4d318d574e77fd6
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-06 16:42:01 -0600

    Update login-script.php

--- public_html/php/login-script.php
+++ public_html/php/login-script.php
@@ -3,7 +3,7 @@
 	// Get the user information provided through POST
 	$email      = $_POST['email'];
 	$plaintext  = $_POST['password'];
-	session_start();
+	
 
 	// Encrypt the provided plaintext password
 	$ciphertext = password_hash($plaintext, PASSWORD_DEFAULT);
@@ -20,12 +20,13 @@
 
 	if($stmt->rowCount()>0)
 	{
-			$results = $stmt->fetchAll();
-			$_SESSION['profArr'] = $results;
-			header( "Location: ../profile.php" );
+		session_start();
+		$results = $stmt->fetchAll();
+		$_SESSION['profArr'] = $results;
+		header( "Location: ../profile.php" );
 	}
 	else
 	{
-		header('Location: ../auth.php');
+		header("Location: ../auth.php");
 	}
 ?>
