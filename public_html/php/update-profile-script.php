36d87e3b5aa769eb3bc63d3854ac997300d8963c
Christian Westbrook <christianwestbrook@live.com>
2018-11-08 15:09:07 -0600

    Test code

--- public_html/php/update-profile-script.php
+++ public_html/php/update-profile-script.php
@@ -2,7 +2,7 @@
     include '..\session.php';
 
     // Get the user information provided through POST
-    $id         = (int) $_SESSION['USER_ID'];
+    $id         = $_SESSION['USER_ID'];
     $fname      = $_POST['fname'];
     $lname      = $_POST['lname'];
     $title      = $_POST['title'];
