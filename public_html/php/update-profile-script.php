0c426aaa41d8c46adb44d6d58399ce3d9de46641
Christian Westbrook <christianwestbrook@live.com>
2018-11-08 15:08:43 -0600

    Test code

--- public_html/php/update-profile-script.php
+++ public_html/php/update-profile-script.php
@@ -2,7 +2,7 @@
     include '..\session.php';
 
     // Get the user information provided through POST
-    $id         = $_SESSION['USER_ID'];
+    $id         = (int) $_SESSION['USER_ID'];
     $fname      = $_POST['fname'];
     $lname      = $_POST['lname'];
     $title      = $_POST['title'];
@@ -14,10 +14,11 @@
 
     if($fname !== '')
     {
-        $sql = 'UPDATE USERS SET FNAME= :FNAME WHERE USER_ID=' . $id;
+        $sql = 'UPDATE USERS SET FNAME= :FNAME WHERE USER_ID= :USER_ID';
 
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':FNAME', $fname, PDO::PARAM_STR);
+        $stmt->bindParam(':USER_ID', $id, PDO::PARAM_INT);
         $stmt->execute();
     }
 
