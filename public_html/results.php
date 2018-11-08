a87b811b9a4f669ea8e3652ae2742c67023cb9d0
Christian Westbrook <christianwestbrook@live.com>
2018-11-08 15:35:35 -0600

    tEST

--- public_html/results.php
+++ public_html/results.php
@@ -21,10 +21,23 @@ include('header.php');
 		<?php
 			include 'php/database.php';
 
-			
+			$search = $_POST['search'];
 
 			$sql = 'SELECT USERS_INTERESTS.USER_ID FROM USERS_INTERESTS INNER JOIN INTERESTS ON USERS_INTERESTS.INT_ID = INTERESTS.INT_ID WHERE INTERESTS.NAME LIKE :SEARCH';
 			$stmt->bindParam(':SEARCH', $search, PDO::PARAM_STR);
+
+			if($stmt->execute())
+			{
+				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				if($results)
+				{
+					foreach ($results as $key)
+					{
+						echo $key['USER_ID'];
+					}
+				}
+			}
 		?>
 	</div>
 </div>
