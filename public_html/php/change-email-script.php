26df3009f55e9a4e288b54b3ff8db9e62f37b4e4
Christian Westbrook <christianwestbrook@live.com>
2018-11-09 02:53:55 -0600

    Updated change-email-script.php

--- public_html/php/change-email-script.php
+++ public_html/php/change-email-script.php
@@ -1,3 +1,33 @@
 <?php
-    header( "Location: ../settings.php" );
+    include '..\session.php';
+
+    if($sessionStarted == false)
+    {
+       header('Location: /~orient/');
+    }
+
+    // Get the user information provided through POST
+	$email      = $_POST['email'];
+	$confirm    = $_POST['confirm-email'];
+    $id         = $_SESSION['USER_ID'];
+
+    if($email == $confirm)
+    {
+        include 'database.php';
+
+        $sql = 'UPDATE USERS SET EMAIL= :EMAIL WHERE USER_ID= :USER_ID';
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':EMAIL', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':USER_ID', $id, PDO::PARAM_STR);
+
+        if($stmt->execute())
+    	{
+            header( "Location: ../settings.php" );
+        }
+        else
+        {
+            echo "Unable to update databse.";
+        }
+    }
 ?>
