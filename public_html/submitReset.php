5e44bfc1abcaad51de323f1ba8d72729a79c88f8
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-29 10:10:54 -0600

    Update submitReset.php

--- public_html/submitReset.php
+++ public_html/submitReset.php
@@ -14,6 +14,7 @@
 		{
 			$subject = "Password reset request";
 			$headers = "From: admin@orient.com";
+			$body = "blah"
 			mail($email,$subject,$body,$headers);
 			header('Location: auth.php');
 		}
@@ -26,32 +27,4 @@
 	{
 		echo "Unable to access the ORIENT database.";
 	}
-	
-	
-	
-
-	$body = <<<HTML
-			<head>
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-			<script>
-			$(document).ready(function(){
-			    $("button").click(function(){
-				$.post("demo_test_post.asp",
-				{
-				  name: "Donald Duck",
-				  city: "Duckburg"
-				},
-				function(data,status){
-				    alert("Data: " + data + "\nStatus: " + status);
-				});
-			    });
-			});
-			</script>
-			</head>
-			<body>
-
-			<button>Send an HTTP POST request to a page and get the result back</button>
-
-			</body>
-			HTML;
 ?>
