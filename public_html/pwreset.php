dd3f3a7b5ea13ac24e58463c939e0de71f39d8b3
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-29 12:16:47 -0600

    Update pwreset.php

--- public_html/pwreset.php
+++ public_html/pwreset.php
@@ -3,6 +3,8 @@ $css = array(
 		0 => 'signup'
 	    );
 include 'header.php';
+$email   = $_POST['email'];
+$userid   = $_POST['userid'];
 ?>
 
 <div id="container">
@@ -10,9 +12,11 @@ include 'header.php';
 		<p id="label">ORIENT</p>
     <p id="label">Reset Password</p>
 		<form action="php/reset-password-script.php" method="post">
+			<input type="hidden" name="email" value="<?php echo $email; ?>">
+			<input type="hidden" name="userid" value="<?php echo $userid; ?>">
 			<input type="password" name="password" placeholder="New Password" class="field" /></br>
 			<input type="password" name="confirm" placeholder="New Password Again" class="field" /></br>
-			<input type="submit" value="Sign Up" id="sub-button">
+			<input type="submit" value="Reset" id="sub-button">
 		</form>
 	</div>
 </div>
