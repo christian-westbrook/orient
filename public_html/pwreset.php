cf5a5316de8673dec647299bc81abaaee86f61e5
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-29 12:11:25 -0600

    Update pwreset.php

--- public_html/pwreset.php
+++ public_html/pwreset.php
@@ -1,3 +1,19 @@
 <?php
-echo "winning!";
+$css = array(
+		0 => 'signup'
+	    );
+include 'header.php';
 ?>
+
+<div id="container">
+	<div id="auth">
+		<p id="label">ORIENT</p>
+    <p id="label">Reset Password</p>
+		<form action="php/reset-password-script.php" method="post">
+			<input type="password" name="password" placeholder="New Password" class="field" /></br>
+			<input type="password" name="confirm" placeholder="New Password Again" class="field" /></br>
+			<input type="submit" value="Sign Up" id="sub-button">
+		</form>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
