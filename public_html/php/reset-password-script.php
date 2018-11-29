b3016bead340f5e3f46815431d3884fac1ee7162
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-29 12:26:57 -0600

    Update reset-password-script.php

--- public_html/php/reset-password-script.php
+++ public_html/php/reset-password-script.php
@@ -1,1 +1,31 @@
-
+<?php
+	$email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirm = $_POST['confirm'];
+  $userid = $_POST['userid'];
+  include 'database.php';
+	$sql  = 'SELECT * FROM USERS WHERE USER_ID= :USER_ID AND EMAIL= :EMAIL';
+  $stmt = $conn->prepare($sql);
+	$stmt->bindParam(':USER_ID', $userid, PDO::PARAM_INT);
+  $stmt->bindParam(':EMAIL', $email, PDO::PARAM_STR);
+  if($stmt->execute())
+	{
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if($results && ($password == $confirm))
+		{
+            $ciphertext = password_hash($password, PASSWORD_DEFAULT);
+            $sql = 'UPDATE USERS SET PASSWORD= :PASSWORD WHERE USER_ID= :USER_ID';
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':USER_ID', $userid, PDO::PARAM_STR);
+            $stmt->bindParam(':PASSWORD', $ciphertext, PDO::PARAM_STR);
+            if($stmt->execute())
+        	  {
+                header( "Location: ../settings.php" );
+            }
+            else
+            {
+                echo "Unable to update database.";
+            }
+        }
+    }
+?>
