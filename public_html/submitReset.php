66a534a2010e205d99e8af8784fa5b0cef6f1c5f
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-11-29 09:58:47 -0600

    Update submitReset.php

--- public_html/submitReset.php
+++ public_html/submitReset.php
@@ -4,10 +4,55 @@
 		echo "error! you need to submit the form!";
 	}
 	$email = $_POST['email'];
-	$link = "espn.com";
-	$subject = "Password reset request";
-	$body = "Here is the link to reset your password.\n\n".$link;
-	$headers = "From: overlords@orient.com";
-	mail($email,$subject,$body,$headers);
-	header('Location: auth.php');
+	include 'database.php';
+	$sql  = 'SELECT * USERS WHERE EMAIL= :EMAIL';
+	$stmt = $conn->prepare($sql);
+	$stmt->bindParam(':EMAIL', $email, PDO::PARAM_STR)
+	if($stmt->execute())
+	{
+		if($stmt->rowCount()>0)
+		{
+			$subject = "Password reset request";
+			$headers = "From: admin@orient.com";
+			mail($email,$subject,$body,$headers);
+			header('Location: auth.php');
+		}
+		else
+		{
+			header( "Location: ../auth.php" );
+		}
+	}
+	else
+	{
+		echo "Unable to access the ORIENT database.";
+	}
+	
+	
+	
+
+	$body = "<!DOCTYPE html>
+			<html>
+			<head>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script>
+			$(document).ready(function(){
+			    $("button").click(function(){
+				$.post("demo_test_post.asp",
+				{
+				  name: "Donald Duck",
+				  city: "Duckburg"
+				},
+				function(data,status){
+				    alert("Data: " + data + "\nStatus: " + status);
+				});
+			    });
+			});
+			</script>
+			</head>
+			<body>
+
+			<button>Send an HTTP POST request to a page and get the result back</button>
+
+			</body>
+			</html>";
 ?>
