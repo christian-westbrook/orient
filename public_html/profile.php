d540d5405b319a8df5af6ec6ccb118c44c21b33e
Christian Westbrook <christianwestbrook@live.com>
2018-11-30 01:07:21 -0600

    Updated profile.php

--- public_html/profile.php
+++ public_html/profile.php
@@ -169,7 +169,7 @@ $_POST['DEP'] = $dep;
 					$length = count($skills);
 					for($i = 0; $i < $length; $i++)
 					{
-						echo $skills[$i] . " ";
+						echo '<a href="results.php?SEARCH="' . $skills[$i] . '">' . $skills[$i] . '</a> ';
 					}
 				?>
 				</p>
@@ -181,7 +181,7 @@ $_POST['DEP'] = $dep;
 					$length = count($interests);
 					for($i = 0; $i < $length; $i++)
 					{
-						echo '<a href="results.php?SEARCH="' . $interests[$i] . '">' . $interests[$i] . '</a>   ';
+						echo '<a href="results.php?SEARCH="' . $interests[$i] . '">' . $interests[$i] . '</a> ';
 					}
 				?>
 				</p>
