b28ba411f7f4316b7fe46db5dedfc912c043da09
Christian Westbrook <christianwestbrook@live.com>
2018-11-30 00:54:09 -0600

    Updated test code in profile.php

--- public_html/profile.php
+++ public_html/profile.php
@@ -134,8 +134,8 @@ if($stmt->execute())
 $sql = "SELECT NAME FROM DEPARTMENTS WHERE DEP_ID = :DEP_ID";
 $stmt = $conn->prepare($sql);
 $dep_id = (int) $_POST['DEP_ID'];
-echo "HERE";
 $stmt->bindParam(':ID', $dep_id, PDO::PARAM_INT);
+echo "HERE";
 
 $dep = '';
 if($stmt->execute())
