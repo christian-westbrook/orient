e60029e1c5edb70fc0872d11f4f72824daf285dd
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-12-01 17:06:42 -0600

    Update admin-settings.php

--- public_html/admin-settings.php
+++ public_html/admin-settings.php
@@ -41,6 +41,8 @@ $stmtNU->execute();
 <div id="container">
 	<div id="settings">
 		<p id="heading">Settings</p>
+		
+		
 		<select name="newuserid" class="field">
 		<option value="ignore">Select A User</option>
 		<?php
@@ -51,34 +53,23 @@ $stmtNU->execute();
 		<option value="<?php echo $valNU[$i]['USER_ID'];?>"><?php echo $valNU[$i]['FNAME']." ".$valNU[$i]['LNAME'];?></option>
 		<?php } ?>
 		</select><br>
-			
-		<p class="label">Change Email</p>
-
-		<form action="php/change-email-script.php" method="POST">
+		<br>
+		<form action="php/admin-profile-script.php" method="POST">
+		
 			<input type="text" name="email" placeholder="New Email" class="field" /></br>
 			<input type="text" name="confirm-email" placeholder="Confirm" class="field" /></br>
 			<input type="submit" value="Change Email" class="sub-button">
-		</form>
-
-		<p class="label">Change Password</p>
-
-		<form action="php/change-password-script.php" method="POST">
+		
 			<input type="password" name="old-password" placeholder="Old Password" class="field" /></br>
 			<input type="password" name="new-password" placeholder="New Password" class="field" /></br>
 			<input type="password" name="confirm-password" placeholder="Confirm" class="field" /></br>
 			<input type="submit" value="Change Password" class="sub-button">
-		</form>
+		
 
-		<p class="label">Update Profile Picture</p>
-
-		<form action="php/update-profile-picture-script.php" method="POST">
 			<input type="file" class="field" name="profile" accept="image/*" /></br>
 			<input type="submit" value="Update Profile Picture" class="sub-button" name="pic-sub">
-		</form>
 
-		<p class="label">Update Profile Information</p>
 
-		<form action="php/update-profile-script.php" method="POST">
 			<input type="text" name="fname" placeholder="First Name" class="field" /></br>
 			<input type="text" name="lname" placeholder="Last Name" class="field" /></br>
 			<input type="text" name="title" placeholder="Title" class="field" /></br>
