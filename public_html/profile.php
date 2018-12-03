14dd2cd7913c5b1b2f84d661a289ff0eb978c14a
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 00:22:46 -0600

    Updated profile.php

--- public_html/profile.php
+++ public_html/profile.php
@@ -191,7 +191,35 @@ function formatDateTime($datetime)
 	$month = substr($datetime, 5, 2);
 	$day = substr($datetime, 8, 2);
 
-	$formatted = $year . ' ' . $month . ' ' . $day;
+	$switch ($month)
+	{
+		case '01': 	$month = 'Jan.';
+		 			break;
+		case '02': $month = 'Feb.';
+					break;
+		case '03': $month = 'Mar.';
+					break;
+		case '04': $month = 'Apr.';
+					break;
+		case '05':	$month = 'May';
+					break;
+		case '06':	$month = 'June';
+					break;
+		case '07': 	$month = 'July';
+					break;
+		case '08':	$month = "Aug.";
+					break;
+		case '09':	$month = "Sep.";
+					break;
+		case '10':	$month = "Oct.";
+					break;
+		case '11':	$month = "Nov.";
+					break;
+		case '12':	$month = "Dec.";
+					break;
+	}
+
+	$formatted = $month . ' ' . $day . ', ' . $year;
 	return $formatted;
 }
 
