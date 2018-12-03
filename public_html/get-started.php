dbcca02b2517f480c6fd4fb7d2355c959744919c
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 04:25:47 -0600

    Updated get-started styles

--- public_html/get-started.php
+++ public_html/get-started.php
@@ -17,17 +17,17 @@ include 'header.php';
 	<div id="types">
 		<div id="admin" class="type">
 			<p class="label"><b>Campus Administrator</b></p>
-			<p>If your University has yet to join ORIENT, find out more about how a research interest network can benefit your faculty, or sign up now.</p>
+			<p class="def">If your University has yet to join ORIENT, find out more about how a research interest network can benefit your faculty, or sign up now.</p>
 		</div>
 
 		<div id="member" class="type">
 			<p class="label"><b>Join your ORIENT network</b></p>
-			<p>If it's your first time here and your campus is already on ORIENT, sign up as a member here.</p>
+			<p class="def">If it's your first time here and your campus is already on ORIENT, sign up as a member here.</p>
 		</div>
 
 		<div id="guest" class="type">
 			<p class="label"><b>Join as a guest</b></p>
-			<p>If you're just here to look around, sign up as a guest.</p>
+			<p class="def">If you're just here to look around, sign up as a guest.</p>
 		</div>
 	</div>
 </div>
