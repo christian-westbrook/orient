e523fc1dbfb5ea7cd58d7bb37f110782d2d20a7d
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 03:55:53 -0600

    Updated get-started.php

--- public_html/get-started.php
+++ public_html/get-started.php
@@ -14,16 +14,21 @@ include 'header.php';
 	<div id="promo">
 	</div>
 
-	<div id="admin" class="type">
-		<p class="label"><b>Organization-wide Collaboration</b></p>
-		<p>If your University or other organization has yet to join ORIENT</p>
-	</div>
+	<div id="types">
+		<div id="admin" class="type">
+			<p class="label"><b>Campus-wide Collaboration</b></p>
+			<p>If your University or other organization has yet to join ORIENT, find out more about how a research interest network can benefit you, or sign up now.</p>
+		</div>
 
-	<div id="member" class="type">
-	</div>
+		<div id="member" class="type">
+			<p class="label"><b>Join your ORIENT network</b></p>
+			<p>If it's your first time here and your organization is already on ORIENT, sign up as a member here.</p>
+		</div>
 
-	<div id="guest" class="type">
+		<div id="guest" class="type">
+			<p class="label"><b>Join as a guest</b></p>
+			<p>If you're just here to look around, sign up as a guest.</p>
+		</div>
 	</div>
-</div>
 
 <?php include 'footer.php'; ?>
