b1d8ad750269e1f43793e36d5c51733166b7fd7c
Christian Westbrook <christianwestbrook@live.com>
2018-12-02 23:49:47 -0600

    Bug fix in make-post-script.php

--- public_html/php/make-post-script.php
+++ public_html/php/make-post-script.php
@@ -10,7 +10,7 @@
 	$sql = 'INSERT INTO POSTS (USER_ID, MESSAGE) VALUES (:USER_ID, :MESSAGE)';
 	$stmt = $conn->prepare($sql);
 	$stmt->bindParam(':USER_ID', $userid, PDO::PARAM_INT);
-	$stmt->bindParam(':MESSAGE', $post, PDO::PARAM_INT);
+	$stmt->bindParam(':MESSAGE', $post, PDO::PARAM_STR);
 
 	if($stmt->execute())
 	{
