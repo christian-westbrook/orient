24048d915b05f380027011861337572f76d19be9
Christian Westbrook <christianwestbrook@live.com>
2018-12-02 23:50:11 -0600

    Updated make-post-script.php

--- public_html/php/make-post-script.php
+++ public_html/php/make-post-script.php
@@ -7,10 +7,10 @@
 	echo $post;
 
 	include 'database.php';
-	$sql = 'INSERT INTO POSTS (USER_ID, MESSAGE) VALUES (:USER_ID, :MESSAGE)';
+	$sql = 'INSERT INTO POSTS (USER_ID, MESSAGE) VALUES (:USER_ID, :POST)';
 	$stmt = $conn->prepare($sql);
 	$stmt->bindParam(':USER_ID', $userid, PDO::PARAM_INT);
-	$stmt->bindParam(':MESSAGE', $post, PDO::PARAM_STR);
+	$stmt->bindParam(':POST', $post, PDO::PARAM_STR);
 
 	if($stmt->execute())
 	{
