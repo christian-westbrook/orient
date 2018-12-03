13f3d0fb5def0090879ec19b187cc2e4b9a21626
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 02:54:06 -0600

    Updated profile pic script

--- public_html/php/update-profile-picture-script.php
+++ public_html/php/update-profile-picture-script.php
@@ -5,7 +5,7 @@
     $id             = $_SESSION['USER_ID'];
     $profile        = $_POST['profile'];
 
-    $print_r($_FILES);
+    print_r($_FILES);
 
     if(is_uploaded_file($_FILES['profile']['tmp_name']) && (substr($_FILES['profile']['type'], 0, 6) == 'image/'))
     {
