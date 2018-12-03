59c0ecffc02b4441c47ec4f61b812aa5db215d2d
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-12-03 10:32:05 -0600

    Update signup-script.php

--- public_html/php/signup-script.php
+++ public_html/php/signup-script.php
@@ -3,10 +3,6 @@ include '../session.php';
 
 if($_POST['password'] == $_POST['confirm'])
 {
-	// do something with the request
-  	//are we making the user automatically i.e. sql insert statement
-  	//are we sending email to admins to confirm before doing sql insert
-  	//in either case, there has to somehow be a dept_id (this could be used as a means to confirm or not)
 
 	// Get the user information provided through POST
   	$email 		= $_POST['email'];
