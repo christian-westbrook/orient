2578614f1e9fe4a3ece2b52810ca0948271cf6a9
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 04:30:37 -0600

    Updated get-started.php

--- public_html/get-started.php
+++ public_html/get-started.php
@@ -16,18 +16,26 @@ include 'header.php';
 
 	<div id="types">
 		<div id="admin" class="type">
-			<p class="label"><b>Campus Administrator</b></p>
+			<p class="label"><b>Campus Administrators</b></p>
 			<p class="type-def">If your University has yet to join ORIENT, find out more about how a research interest network can benefit your faculty, or sign up now.</p>
 		</div>
 
 		<div id="member" class="type">
-			<p class="label"><b>Join your ORIENT network</b></p>
+			<p class="label"><b>Students and Faculty</b></p>
 			<p class="type-def">If it's your first time here and your campus is already on ORIENT, sign up as a member here.</p>
 		</div>
 
 		<div id="guest" class="type">
-			<p class="label"><b>Join as a guest</b></p>
+			<p class="label"><b>Guests</b></p>
 			<p class="type-def">If you're just here to look around, sign up as a guest.</p>
+		</div>
+	</div>
+
+	<div id="action">
+		<div id="Discover ORIENT">
+		</div>
+
+		<div id="Join ORIENT">
 		</div>
 	</div>
 </div>
