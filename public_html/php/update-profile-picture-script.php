89076f270c19ccdb312fbdee27d139d8b9358e18
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 02:34:05 -0600

    Updated profile pic script

--- public_html/php/update-profile-picture-script.php
+++ public_html/php/update-profile-picture-script.php
@@ -11,9 +11,9 @@
 
         include 'database.php';
         $sql = 'UPDATE USERS SET PROFILE= :PROFILE WHERE USER_ID= :USER_ID';
-        echo "HERE";
         $stmt->bindParam(':PROFILE', $target, PDO::PARAM_STR);
         $stmt->bindParam(':USER_ID', $id, PDO::PARAM_INT);
+        echo "HERE";
         $stmt->execute();
 
         if(move_uploaded_file($_FILES['profile']['tmp_name'], $target))
