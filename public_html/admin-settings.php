dd3a814a62cf7abb63e85d223414fc221ffc0ecc
bhamil00 <44686184+bhamil00@users.noreply.github.com>
2018-12-03 14:19:41 -0600

    Update admin-settings.php

--- public_html/admin-settings.php
+++ public_html/admin-settings.php
@@ -78,17 +78,11 @@ $valCU = $stmtCU->fetchAll(PDO::FETCH_ASSOC);
 			<input type="file" class="field" name="profile" accept="image/*" /></br>
 			<input type="submit" value="Update Profile Picture" class="sub-button" name="pic-sub"></br>
 
-			<label>First Name:</label></br>
 			<input type="text" name="fname" value="<?php echo $valCU[0]['FNAME'];?>" class="field" /></br>
-			<label>Last Name:</label></br>
 			<input type="text" name="lname" value="<?php echo $valCU[0]['LNAME'];?>" class="field" /></br>
-			<label>Email:</label></br>
 			<input type="text" name="email" value="<?php echo $valCU[0]['EMAIL'];?>" class="field" /></br>
-			<label>Title:</label></br>
 			<input type="text" name="title" value="<?php echo $valCU[0]['TITLE'];?>" class="field" /></br>
-			<label>Hometown:</label></br>
 			<input type="text" name="hometown" value="<?php echo $valCU[0]['HOMETOWN'];?>" class="field" /></br>
-			<label>Phone Num:</label></br>
 			<input type="text" name="phone-num" value="<?php echo $valCU[0]['PHONE_NUM'];?>" class="field" /></br>
 			<input type="textarea" rows="4" wrap="hard" name="bio" placeholder="Research Summary" class="field" id="research-summary"/></br>
 			<select name="role" class="field">
