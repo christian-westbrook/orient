51623895f8d7e6aa265d496928227d67653caffc
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 03:39:59 -0600

    Updated get-started styles

--- public_html/get-started.php
+++ public_html/get-started.php
@@ -9,7 +9,19 @@ include 'header.php';
 ?>
 
 <div id="container">
+	<h1 id="head-text">There's a place for everyone here.</h1>
 
+	<div id="promo">
+	</div>
+
+	<div id="admin">
+	</div>
+
+	<div id="member">
+	</div>
+
+	<div id="guest">
+	</div>
 </div>
 
 <?php include 'footer.php'; ?>
