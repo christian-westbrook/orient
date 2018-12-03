38bd4469a1a9c1ba4600c48b079c3dab65a24cbc
Christian Westbrook <christianwestbrook@live.com>
2018-12-03 02:23:44 -0600

    Updated profile pic script

--- public_html/php/update-profile-picture-script.php
+++ public_html/php/update-profile-picture-script.php
@@ -8,7 +8,15 @@
     if(is_uploaded_file($_FILES['profile']['tmp_name']) && (substr($_FILES['profile']['type'], 0, 6) == 'image/'))
     {
         $target = '../img/users/' . $id . '.' . substr($_FILES['profile']['type'], 6);
-        echo $target;
-        //move_uploaded_file($_FILES['profile']['tmp_name'], $target);
+
+        include 'database.php';
+        $sql = 'UPDATE USERS SET PROFILE= :PROFILE WHERE USER_ID= :USER_ID';
+        $stmt->bindParam(':PROFILE', $target, PDO::PARAM_STR);
+        $stmt->bindParam(':USER_ID', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        move_uploaded_file($_FILES['profile']['tmp_name'], $target);
     }
+
+    header( "Location: ../profile.php" );
 ?>
