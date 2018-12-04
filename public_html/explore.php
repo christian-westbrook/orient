330ba1caaaaf650b4450ca502411e12a3cde4a9c
Christian Westbrook <christianwestbrook@live.com>
2018-12-04 00:45:47 -0600

    Updated explore

--- public_html/explore.php
+++ public_html/explore.php
@@ -6,6 +6,46 @@ $css = array(
 
 include 'header.php';
 
+// Time formatter function
+function formatDateTime($datetime)
+{
+	$year = substr($datetime, 0, 4);
+	$month = substr($datetime, 5, 2);
+	$day = substr($datetime, 8, 2);
+	$time = substr($datetime, 11, 8);
+
+	switch ($month)
+	{
+		case "01": 	$month = 'Jan.';
+		 			break;
+		case "02": $month = 'Feb.';
+					break;
+		case "03": $month = 'Mar.';
+					break;
+		case "04": $month = 'Apr.';
+					break;
+		case "05":	$month = 'May';
+					break;
+		case "06":	$month = 'June';
+					break;
+		case "07": 	$month = 'July';
+					break;
+		case "08":	$month = "Aug.";
+					break;
+		case "09":	$month = "Sep.";
+					break;
+		case "10":	$month = "Oct.";
+					break;
+		case "11":	$month = "Nov.";
+					break;
+		case "12":	$month = "Dec.";
+					break;
+	}
+
+	$formatted = $month . ' ' . $day . ', ' . $year . ' ' . $time;
+	return $formatted;
+}
+
 ?>
 
 <body>
@@ -13,7 +53,7 @@ include 'header.php';
 	<?php
 
 	include 'php/database.php';
-	$sql = "SELECT USERS.USER_ID, USERS.TITLE, USERS.FNAME, USERS.LNAME, USERS.PROFILE, POSTS.MESSAGE FROM POSTS INNER JOIN USERS ON USERS.USER_ID = POSTS.USER_ID";
+	$sql = "SELECT USERS.USER_ID, USERS.TITLE, USERS.FNAME, USERS.LNAME, USERS.PROFILE, POSTS.MESSAGE, POSTS.CREATE_TIME FROM POSTS INNER JOIN USERS ON USERS.USER_ID = POSTS.USER_ID";
 	$stmt = $conn->prepare($sql);
 
 	if($stmt->execute())
@@ -27,9 +67,10 @@ include 'header.php';
 				$id = $results[$key]["USER_ID"];
 				$name = $results[$key]["TITLE"] . ' ' . $results[$key]["FNAME"] . ' ' . $results[$key]["LNAME"];
 				$profile = $results[$key]["PROFILE"];
-				$post= $results[$key]["MESSAGE"];
-				echo '<div class="post"><img class="post-pic" src="' . $profile . '" /><a href="profile.php?SEARCH_ID=' . $id . '"><p class="post-name">' . $name . '</p></a><p class="post-message">' . $post . '</p></div>';
-			}
+				$post = $results[$key]["MESSAGE"];
+				$datetime = $results[$key]["CREATE_TIME"];
+				$time = formatDateTime($datetime);
+				echo '<div class="post"><img class="post-profile-pic" src="' . $profile . '" /> <span class="post-name">' . $name . '</span></br><span class="post-message">' . $post . '</span></br><span class="post-time">' . $time . '</span></div></br></br>';
 		}
 	}
 	else
